759c6c1c3b189e1f7f4536471d4cdb928b78fc09
Aram Avakyan <avakanaram20@gmail.com>
2024-09-14 14:41:47 +0400

    refactoring

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
 import { useEffect, useState } from "react";
 import styles from "./app.module.css";
-// Добавить кнопку добавить дело.
+import { editTodo, addTodo, deleteTodo } from "./handlers";
+import { alphabedSort } from "./utils";
+
 function App() {
   const [todos, setTodos] = useState([]);
   const [refreshTodosFlag, setRefreshTodosFlag] = useState(false);
   const [value, setValue] = useState("");
+
+  const toggleRefreshTodosFlag = () => {
+    setRefreshTodosFlag(!refreshTodosFlag);
+  };
 
   useEffect(() => {
     fetch("http://localhost:3005/todos")
@@ -14,90 +20,58 @@ function App() {
       });
   }, [refreshTodosFlag]);
 
-  const addTodo = () => {
-    const userTodo = prompt("Введите todo");
-    if (userTodo !== "" && userTodo !== null) {
-      fetch("http://localhost:3005/todos", {
-        method: "POST",
-        headers: { "Contentgjr-Type": "application/json;charset=utf-8" },
-        body: JSON.stringify({
-          content: userTodo,
-        }),
-      })
-        .then((rawResponse) => {
-          rawResponse.json();
-          console.log(rawResponse);
-        })
-        .then(() => {
-          console.log("Ответ от сервера: todo добавлен: ");
-        })
-        .finally(() => setRefreshTodosFlag(!refreshTodosFlag));
-    }
+  const handleChangeTodo = (event) => {
+    setValue(event.target.value);
   };
-
-  const deleteTodo = (id) => {
-    fetch(`http://localhost:3005/todos/${id}`, {
-      method: "DELETE",
-    })
-      .then((rawResponce) => rawResponce.json())
-      .then((responce) => console.log(responce))
-      .finally(() => setRefreshTodosFlag(!refreshTodosFlag));
-  };
-
-  const edtiTodo = (id) => {
-    const userTodo = prompt("Введите текст");
-    if (userTodo !== "" && userTodo !== null) {
-      fetch(`http://localhost:3005/todos/${id}`, {
-        method: "PUT",
-        headers: { "Content-Type": "application/json;charset=utf-8" },
-        body: JSON.stringify({
-          content: userTodo,
-        }),
-      })
-        .then((rawResponce) => rawResponce.json())
-        .then((responce) => console.log(responce))
-        .finally(() => setRefreshTodosFlag(!refreshTodosFlag));
-    }
-  };
-
-  // const findTodo = () => {
-  //   for (i = 0; i < todos.length(); i++) {
-  //     if (value.includes(todos[i].content)) {
-  //       console.log("Привет");
-  //     }
-  //   }
-  // };
 
   return (
     <>
       <div className={styles.todosContainer}>
         <div className={styles.headerContent}>
           <span className={styles.title}>Todo List</span>
-          <button className={styles.btnAdd} onClick={addTodo}>
+          <button
+            className={styles.alphabedBtn}
+            onClick={() => alphabedSort(todos, setTodos)}
+          >
+            A⬇Z
+          </button>
+          <button
+            className={styles.btnAdd}
+            onClick={() => addTodo(toggleRefreshTodosFlag)}
+          >
             🞢
           </button>
-          {/* <input value={value} onChange={findTodo} />
-        </div> */}
-        {todos.map(({ id, content }) => (
-          <li className={styles.todoWrapper} key={id}>
-            <input type="checkbox" />
-            {content}
-            <button
-              onClick={() => deleteTodo(id)}
-              className={styles.deleteTodoBtn}
-            >
-              ❌
-            </button>
-            <button
-              className={styles.editBtn}
-              onClick={() => {
-                edtiTodo(id);
-              }}
-            >
-              &#x1F589;
-            </button>
-          </li>
-        ))}
+          <span className={styles.findTodoLabel}>Поиск </span>
+          <input
+            className={styles.findTodoInput}
+            value={value}
+            onChange={handleChangeTodo}
+          />
+        </div>
+        {todos
+          .filter(({ content }) =>
+            content.toLowerCase().includes(value.toLowerCase())
+          )
+          .map(({ id, content }) => (
+            <li className={styles.todoWrapper} key={id}>
+              <input type="checkbox" />
+              {content}
+              <button
+                onClick={() => deleteTodo(id, toggleRefreshTodosFlag)}
+                className={styles.deleteTodoBtn}
+              >
+                ❌
+              </button>
+              <button
+                className={styles.editBtn}
+                onClick={() => {
+                  editTodo(id, content, toggleRefreshTodosFlag);
+                }}
+              >
+                &#x1F589;
+              </button>
+            </li>
+          ))}
       </div>
     </>
   );
